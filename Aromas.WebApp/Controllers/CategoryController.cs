729f311eca98bc101dc0fb4955dec582446f6f04
José Ricardo <zericardobraga@gmail.com>
2021-09-25 17:28:00 -0300

    criado os arquivos da controller (category e product)

---
+++ Aromas.WebApp/Controllers/CategoryController.cs
@@ -0,0 +1,38 @@
+﻿using Aromas.App.Interface;
+using Aromas.Domain.Entities;
+using Aromas.MVC.ViewModels;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aromas.MVC.Controllers
+{
+    public class CategoryController
+    {
+        readonly IMapper _mapper;
+        private readonly ICategoryAppService _categoryAppService;
+
+        public CategoryController(ICategoryAppService categoryAppService, IMapper mapper) 
+        {
+            _categoryAppService = categoryAppService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [Route("Category")]
+        public ActionResult Index()
+        {
+            var categories = _categoryAppService.GetAll();
+            var model = _mapper.Map<IEnumerable<CategoryViewModel>>(categories);
+
+            return View(model);
+        }
+
+
+
+
+    }
+}
